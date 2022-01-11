253684434e01049b5a53f4304f27187368e3f0f5
michelet <michelet@usp.br>
2022-01-11 11:52:27 -0300

    WIP - succesful codegen

--- src/Sushi/mappings/KashiPairMediumRiskV1.ts
+++
@@ -1,6 +0,0 @@
-import { Transfer as TransferEvent } from "../../../generated/SushiKashiPairMediumRiskV1/SushiKashiPairMediumRiskV1";
-import { handleKashiPair } from "./handlers";
-
-export function handleTransfer(event: TransferEvent): void {
-  handleKashiPair(event.transaction.hash, event.block.number, event.block.timestamp, event.address);
-}
--- src/Sushi/mappings/MiniChefV2.ts
+++
@@ -1,6 +0,0 @@
-import { LogUpdatePool as LogUpdatePoolEvent } from "../../../generated/SushiMiniChefV2/SushiMiniChefV2";
-import { handlePool } from "./handlers";
-
-export function handleLogUpdatePool(event: LogUpdatePoolEvent): void {
-  handlePool(event.transaction.hash, event.block.number, event.block.timestamp, event.params.pid);
-}
