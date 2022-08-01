8ed3737c052aa8b33176f49a4004c169e63a916b
michelet <michelet@usp.br>
2022-08-01 16:21:15 -0300

    updating masterchefv1 event names.

--- src/Sushi/mappings/MasterChef.ts
+++ src/Sushi/mappings/MasterChef.ts
@@ -4,14 +4,22 @@ import {
   Withdraw as WithdrawEvent,
   EmergencyWithdraw as EmergencyWithdrawEvent,
 } from "../../../generated/SushiMasterChef/SushiMasterChef";
+import { ZERO_BI } from "../../utils/constants";
 
 export function handleDeposit(event: DepositEvent): void {
+  let eventType = "";
+  if (event.params.amount == ZERO_BI) {
+    eventType = "LPS_RewardAdded";
+  } else {
+    eventType = "LPS_Staked";
+  }
+
   handlePoolV1(
     event.transaction.hash,
     event.block.number,
     event.block.timestamp,
     event.params.pid,
-    "deposit",
+    eventType,
     event.params.amount,
     event.transactionLogIndex,
     event.transaction.gasUsed,
@@ -19,25 +27,18 @@ export function handleDeposit(event: DepositEvent): void {
   );
 }
 export function handleWithdraw(event: WithdrawEvent): void {
+  let eventType = "";
+  if (event.params.amount == ZERO_BI) {
+    eventType = "LPS_RewardPaid";
+  } else {
+    eventType = "LPS_Withdrawn";
+  }
   handlePoolV1(
     event.transaction.hash,
     event.block.number,
     event.block.timestamp,
     event.params.pid,
-    "withdraw",
-    event.params.amount,
-    event.transactionLogIndex,
-    event.transaction.gasUsed,
-    event.transaction.gasPrice,
-  );
-}
-export function handleEmergencyWithdraw(event: EmergencyWithdrawEvent): void {
-  handlePoolV1(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.params.pid,
-    "emergencyWithdraw",
+    eventType,
     event.params.amount,
     event.transactionLogIndex,
     event.transaction.gasUsed,
